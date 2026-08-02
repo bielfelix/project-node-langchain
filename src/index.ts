a8066681655856baf55c0f24b76c5406290bd953
Gabriel Felix <55044310+bielfelix@users.noreply.github.com>
2026-08-01 22:04:17 -0300

    connection with LangChain

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+
+import { createServer } from "./server.ts";
+
+const app = createServer()
+const host = '0.0.0.0'
+const port = Number(process.env.PORT ?? 3000)
+
+try {
+  await app.listen({ port, host })
+  app.log.info(`Server listening on port ${port}`)
+} catch (error: unknown) {
+  if (error && typeof error === 'object' && 'code' in error && (error as { code?: string }).code === 'EADDRINUSE') {
+    await app.listen({ port: 0, host })
+    app.log.info('Server listening on a random available port')
+  } else {
+    throw error
+  }
+}
+console.log(`Server listening on port ${port}`)
+
+// console.log('test')
+
+// await app.listen({ port: 3000, host: '0.0.0.0' })
+
+// app.inject({
+//   method: 'POST',
+//   url: '/chat',
+//   body: { question: 'What is the capital of France?'}
+// }).then((response) => {
+//   console.log('Response status: ', response.statusCode)
+//   console.log('Response body: ', response.body)
+// })
---
+++ tests/router.e2e.test.ts
@@ -0,0 +1,21 @@
+import test from 'node:test'
+import assert from 'node:assert/strict'
+import { createServer } from '../src/server.ts'
+
+
+
+test('command upper tranforms message into UPPERCASE', async () => {
+
+    const app = createServer()
+    const msg = 'make THis message UPPER please!'
+    const expected = msg.toUpperCase()
+    const response = await app.inject({
+        method: 'POST',
+        url: '/chat',
+        body: { question: msg}
+    })
+    assert.equal(response.statusCode, 200)
+    assert.equal(response.body, expected)
+
+
+})
